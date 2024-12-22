df1906591ab2a8b9b248c33e35e7ca46b3168dc8
JGONZALEZ <jgonzalez@servicioexterno.bancocaminos.es>
2024-12-22 14:35:51 +0100

    Fixed problems

---
+++ service/src/main/java/es/zed/HexaRx3Application.java
@@ -0,0 +1,12 @@
+package es.zed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaRx3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HexaRx3Application.class, args);
+  }
+}
